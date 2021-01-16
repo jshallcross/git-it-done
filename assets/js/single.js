c806fd76aaf95e4093054cf1c61bca2dc92fc864
Jon Shallcross <jshallcross1@icloud.com>
2021-01-16 11:23:05 -0500

    link html pages

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,5 +1,16 @@
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
+var repoNameEl = document.querySelector("#repo-name");
+
+
+
+var getRepoName = function(){
+    var queryString = document.location.search;
+    var repoName = queryString.split("=")[1];
+    getRepoIssues(repoName);
+    repoNameEl.textContent = repoName;
+};
+
 
 var getRepoIssues = function(repo) {
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
@@ -71,5 +82,5 @@ var displayIssues = function(issues) {
     }
 };
 
-getRepoIssues("Facebook/React");
+getRepoName();
 
