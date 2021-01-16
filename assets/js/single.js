138e6339dab7d3977884411869de368b164374c0
Jon Shallcross <jshallcross1@icloud.com>
2021-01-16 11:30:00 -0500

    error handling

--- assets/js/single.js
+++ assets/js/single.js
@@ -5,10 +5,18 @@ var repoNameEl = document.querySelector("#repo-name");
 
 
 var getRepoName = function(){
+    // grab repo name from url query string
     var queryString = document.location.search;
     var repoName = queryString.split("=")[1];
-    getRepoIssues(repoName);
-    repoNameEl.textContent = repoName;
+
+    if(repoName) {
+        repoNameEl.textContent = repoName;
+
+        getRepoIssues(repoName);  
+    } else {
+        // if no repo was given, redirect to the homepage
+        document.location.replace("./index.html");
+    }
 };
 
 
@@ -29,7 +37,8 @@ var getRepoIssues = function(repo) {
             });
         }
         else {
-            alert("There was a problem with your request!");
+            // if not successful, redirect to homepage
+            document.location.replace("./index.html");
         }
     });      
 };
